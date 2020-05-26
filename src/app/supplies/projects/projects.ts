efaa6ba05a1b541a767f19efd50304c58360caaa
yliang <yliang@Yans-MBP.valhalla>
2020-05-26 15:24:43 -0700

    wave

--- src/app/supplies/projects/projects.ts
+++ src/app/supplies/projects/projects.ts
@@ -6,61 +6,60 @@ export interface Project {
 }
 
 export const PROJECTS: Project[] = [
-
-   {
-      id: 'd',
-      name: 'reading-level',
-      route: 'readability',
-      description: 'find out the approximate grade level needed to comprehend given text'
-   },
-   {
-      id: 'c',
-      name: 'substitution-cipher',
-      route: 'cipher',
-      description: 'encrypt and decipher a message by replacing every letter with another letter'
-   },
-   {
-      id: 'e',
-      name: 'preference-polling',
-      route: 'preference',
-      description: 'compute a polling result that represents the ranked choices of a group'
-   },
+   // {
+   //    id: 'd',
+   //    name: 'reading-level',
+   //    route: 'overview/sandbox/readability',
+   //    description: 'find out the approximate grade level needed to comprehend given text'
+   // },
+   // {
+   //    id: 'c',
+   //    name: 'substitution-cipher',
+   //    route: 'overview/sandbox/cipher',
+   //    description: 'encrypt and decipher a message by replacing every letter with another letter'
+   // },
+   // {
+   //    id: 'e',
+   //    name: 'preference-polling',
+   //    route: 'overview/sandbox/preference',
+   //    description: 'compute a polling result that represents the ranked choices of a group'
+   // },
    {
       id: 'a',
       name: 'idle-journal',
-      route: 'journal',
-      description: 'keep everything in one place and never miss out on anything'
+      route: '/journal',
+      description: 'a place to jot down thoughts and ideas'
    },
-   {
-      id: 'b',
-      name: 'signature-generator',
-      route: 'signature',
-      description: 'create a signature with name, contact info, and social media presence'
-   },
-   {
-      id: 'f',
-      name: 'mix-match',
-      route: 'match',
-      description: 'play mix and match card game'
-   },
+   // {
+   //    id: 'b',
+   //    name: 'signature-generator',
+   //    route: 'overview/sandbox/signature',
+   //    description: 'create a signature with name, contact info, and social media presence'
+   // },
+   // {
+   //    id: 'f',
+   //    name: 'mix-match',
+   //    route: 'overview/sandbox/match',
+   //    description: 'play mix and match card game'
+   // },
    {
       id: 'g',
-      name: 'notes-over',
-      route: 'notes',
-      description: 'jot down notes on any document to be signed off'
+      name: 'a-sandbox',
+      route: 'overview/sandbox/cover',
+      description: 'a few tiny random programs i built for fun'
    },
    {
       id: 'h',
       name: 'architecture-portfolio',
-      route: 'portfolio',
-      description: 'website showcasing architectural design projects over the years built in React.js'
+      route: '/architecture',
+      description: 'a site to catalog my previous architectural design projects'
    },
-   {
-      id: 'i',
-      name: "...",
-      route: '',
-      description: ''
-   },
+   // {
+   //    id: 'i',
+   //    name: "...",
+   //    route: 'overview/sandbox/',
+   //    description: ''
+   // },
 
 
 ];
